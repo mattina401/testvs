400b7b37c9756f7a9f1c8e3e03d7f02b45ed0a85
jongyeon <mattina401@gmail.com>
2014-10-02 16:47:56 -0400

    djsaklds

---
+++ TaskList/TaskList/Controllers/HomeController.vb
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskList.Models;
+
+Namespace TaskList.Controllers
+{
+     public class HomeController : Controller
+     {
+          // 일정 목록을 보여준다.
+          public ActionResult Index()
+          {
+               return View();
+          }
+          // 새 일정을 작성하는 폼을 보여준다.
+          public ActionResult Create()
+          {
+               return View();
+          }
+          // 데이터베이스에 새 일정을 추가한다.
+          public ActionResult CreateNew()
+          {
+               return RedirectToAction("Index");
+          }
+          // 완료된 일정을 표시한다.
+          public ActionResult Complete()
+          {
+               // 데이터베이스 로직
+               return RedirectToAction("Index");
+          }
+     }
+}
---
+++ TaskList/TaskList/Views/Create.aspx
@@ -0,0 +1,19 @@
+﻿
+<%@ Page Language="C#" AutoEventWireup="false" CodeBehind="Create.aspx.cs" Inherits="TaskList.Views.Home.Create" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+     <head runat="server">
+          <title></title>
+     </head>
+     <body>
+          <div>
+               <h1>Add New Task</h1>
+               <form method="post" action="CreateNew">
+                    <label for="description">Task:</label>
+                    <input type="text" name="description" />
+                    <br />
+                    <input type="submit" value="Add Task" />
+               </form>
+          </div>
+     </body>
+</html>
