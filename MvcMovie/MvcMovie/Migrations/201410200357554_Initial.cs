785a3f9da1af671ea1304c19e6c952276b0b9456
mattina401 <mattina401@gmail.com>
2015-05-14 18:15:09 -0400

    dd

---
+++ MvcMovie/MvcMovie/Migrations/201410200357554_Initial.cs
@@ -0,0 +1,30 @@
+namespace MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        ReleaseFate = c.DateTime(nullable: false),
+                        Genre = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ReleaseDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
